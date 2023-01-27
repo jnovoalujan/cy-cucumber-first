bbd5da64e0855eae8c71e20f6d2ed092c79275e7
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2023-01-27 09:01:15 -0500

    rollback changes

--- cypress/e2e/step_definitions/order.js
+++ cypress/e2e/step_definitions/order.js
@@ -5,13 +5,10 @@ Given(
   (productName) => {
     cy.visit("/");
     cy.get("[data-rs-event-name='Select Menu'] > a").click();
-    cy.contains("[title]", productName).parents("a").find("button").click().wait(10000);
+    cy.contains("[title]", productName).parents("a").find("button").click();
   }
 );
 
-When("agrego el producto", ()=> {
-    cy.get("form[id='add-to-cart-form']").within(($form) => {
-        cy.get("#add-to-cart-button").click();
-        cy.root().submit();
-    })
-})
+When("agrego el producto", () => {
+  cy.get("#add-to-cart-button").click();
+});
